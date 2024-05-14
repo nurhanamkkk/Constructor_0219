2eeef971a78c4d46e49b53f12236e946b455cf0f
nurhanamkkk <nurhanamk.30@gmail.com>
2024-05-14 16:14:18 +0700

    membuat fungsi utama

--- chainFunction.cpp
+++ chainFunction.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 class buku
 {
-    
-}
+    string judul;
+
+public: 
+    buku setJudul(string judul)
+    {
+        this->judul = judul; 
+        return *this; // chain function
+    }
+    string getJudul()
+    {
+        return this->judul;
+    }
+
+} bukunya;
+
+int main()
+{}
