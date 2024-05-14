f8ea7cffa4a9eda5f8fa379d9978b2d37ac826bb
nurhanamkkk <nurhanamk.30@gmail.com>
2024-05-14 15:34:30 +0700

    membuat fungsi utama

--- Constructor.cpp
+++ Constructor.cpp
@@ -4,5 +4,41 @@ using namespace std;
 
 class mahasiswa
 {
+public:
+     int nim;
+     string nama;
+
+public: 
+      mahasiswa()
+      {
+        nim = 0;
+        nama = "";
+      };
+};
+    mahasiswa(int iNim)
+    {
+        nim = iNim;
+    };
+
+    mahasiswa(string iNama)
+    {
+        nim = iNim;
+        nama = iNama;
+    };
+
+    void cetak()
+    {
+        {
+          cout << "NIM = " << nim << endl;
+          cout << "Nama = " << nama << endl;
+          cout << endl; 
+        };
+    };
+
+
+
+
+int main ()
+{
     
 }
